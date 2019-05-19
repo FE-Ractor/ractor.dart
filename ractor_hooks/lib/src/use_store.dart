9be7429bb5e331e4110374e4110c7d3abd0ab298
黄斌杰 <akira.binjie@gmail.com>
2019-05-19 23:19:37 +0800

    refactor

--- ractor_hooks/lib/src/use_store.dart
+++ ractor_hooks/lib/src/use_store.dart
@@ -1,25 +1,25 @@
+import 'package:dart_actor/dart_actor.dart';
 import 'package:frhooks/frhooks.dart';
 import 'package:ractor/ractor.dart';
 import 'package:ractor_hooks/ractor_hooks.dart';
 
-S useStore<S extends Store>(S Function() createStore) {
+S useStore<S extends Store>() {
   final currentSystem = useSystem();
   final currentContext = useContext();
 
   assert(currentSystem != null);
   assert(currentContext != null);
 
-  final _store = useMemo(createStore);
+  final storeRef = currentSystem.get(S);
+
+  Store store = storeRef != null
+      ? storeRef.getInstance()
+      : currentSystem.actorOf(S as AbstractActor).getInstance();
+  store.mountStatus = store.mountStatus != null
+      ? store.mountStatus
+      : storeRef != null ? "global" : "local";
 
   useEffect(() {
-    final storeRef = currentSystem.get(_store.runtimeType);
-    Store store = storeRef != null
-        ? storeRef.getInstance()
-        : currentSystem.actorOf(_store).getInstance();
-    store.mountStatus = store.mountStatus != null
-        ? store.mountStatus
-        : storeRef != null ? "global" : "local";
-
     final dispose = store.subscribe(() {
       currentContext.markNeedsBuild();
     });
@@ -31,5 +31,5 @@ S useStore<S extends Store>(S Function() createStore) {
     };
   }, []);
 
-  return _store;
+  return store;
 }
